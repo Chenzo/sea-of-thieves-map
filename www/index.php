3895f2e1f3469f8ee22455950dd071aa7da7681e
Vince Suriani <chenzorama@hotmail.com>
2018-07-11 15:33:47 -0400

    PWA works

--- www/index.php
+++ www/index.php
@@ -85,42 +85,6 @@ $title = "Unofficial Sea Of Thieves Map";
     <script>
 
 
-let deferredPrompt;
-var btnInstall = document.querySelector('#install-button');
-
-window.addEventListener('beforeinstallprompt', (event) => {
-
-    console.log("beforeinstallprompt - showme");
-    // Prevent Chrome <= 67 from automatically showing the prompt
-    event.preventDefault();
-    // Stash the event so it can be triggered later.
-    deferredPrompt = event;
-    // Update the install UI to notify the user app can be installed
-    document.querySelector('#installBut').classList.add("showme");
-});
-
-btnInstall.addEventListener('click', () => {
-
-    document.querySelector('#installBut').classList.remove("showme");
-    // Show the modal add to home screen dialog
-    deferredPrompt.prompt();
-    // Wait for the user to respond to the prompt
-    deferredPrompt.userChoice.then((choice) => {
-      if (choice.outcome === 'accepted') {
-        console.log('User accepted the A2HS prompt');
-      } else {
-        console.log('User dismissed the A2HS prompt');
-      }
-      // Clear the saved prompt since it can't be used again
-      deferredPrompt = null;
-    });
-  });
-
-  window.addEventListener('appinstalled', (evt) => {
-    console.log('a2hs', 'installed');
-  });
-
-
 </script>
 
 
