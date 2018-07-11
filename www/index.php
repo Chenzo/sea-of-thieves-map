1a1275d27491b3991664fe14a00e92901ba9f587
Vince Suriani <chenzorama@hotmail.com>
2018-07-11 08:21:58 -0400

    First attempt failed

--- www/index.php
+++ www/index.php
@@ -7,6 +7,14 @@ $title = "Unofficial Sea Of Thieves Map";
 <!DOCTYPE html>
 <html>
     <head>
+        <meta charset="utf-8">
+        <title><?= $title; ?></title>
+        <meta name="robots" content="INDEX,FOLLOW,NOODP" />
+        <link rel="canonical" href="https://seaofthievesmap.chenzorama.com" />
+        <meta name="description" content="<?= $descr; ?>" />
+        <link rel="canonical" href="https://weather-pwa-sample.firebaseapp.com/final/">
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
         <link rel="manifest" href="manifest.json">
 
         <meta name="mobile-web-app-capable" content="yes">
@@ -17,7 +25,7 @@ $title = "Unofficial Sea Of Thieves Map";
         <meta name="msapplication-navbutton-color" content="#222222">
         <meta name="apple-mobile-web-app-status-bar-style" content="black-translucent">
         <meta name="msapplication-starturl" content="/">
-        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+        
 
         <link rel="apple-touch-icon" sizes="180x180" href="images/favicon/apple-touch-icon.png">
         <link rel="icon" type="image/png" sizes="32x32" href="images/favicon/favicon-32x32.png">
@@ -39,13 +47,11 @@ $title = "Unofficial Sea Of Thieves Map";
                 }); 
             });
         }
+
+        
         </script>
         
-        <meta charset="utf-8">
-        <title><?= $title; ?></title>
-        <meta name="robots" content="INDEX,FOLLOW,NOODP" />
-        <link rel="canonical" href="https://seaofthievesmap.chenzorama.com" />
-        <meta name="description" content="<?= $descr; ?>" />
+        
 
 
         <?php include "includes/gtm.php"; ?>
--- www/sw.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-
-importScripts('/js/vendor/sw-toolbox.js');
-toolbox.precache(['index.php','style/style.css']);
-toolbox.router.get('/images/*', toolbox.cacheFirst);
-toolbox.router.get('/*', toolbox.networkFirst, { 
-    networkTimeoutSeconds: 5
-});
