7ffcc923be4be56661add64bc1905f7f3b6721f1
Vince Suriani <chenzorama@hotmail.com>
2018-10-05 15:03:29 -0400

    Docs

--- www/includes/globals.php
+++ www/includes/globals.php
@@ -2,7 +2,7 @@
 $descr = "An open source dynamic map of the sea from the Rare's game Sea of Thieves. Right click to find closest animals, custom sharable markers, mobile friendly / progressive web app and more";
 $title = "Unofficial Sea Of Thieves Map";
 
-define('CACHE_BUSTER', '1538764596162');            // Automatically updates
+define('CACHE_BUSTER', '1538765645481');            // Automatically updates
 
 
 ?>
