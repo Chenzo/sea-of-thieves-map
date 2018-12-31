9cd78287fdb9d8352dbd9c2bf5614d879030b6fe
Vince Suriani <chenzorama@hotmail.com>
2018-12-31 12:45:37 -0500

    Have fix

--- docs/notes.md
+++ docs/notes.md
@@ -1,2 +1,7 @@
 https://leafletjs.com/examples/crs-simple/crs-simple.html
 
+
+generate the file list for the manifest
+find . -type f >> list2.txt
+
+
--- www/includes/globals.php
+++ www/includes/globals.php
@@ -2,9 +2,9 @@
 
 $descr = "An open source interactive map of the sea from the Rare's game Sea of Thieves. Right click to find closest animals, custom sharable markers, mobile friendly / progressive web app and more";
 $title = "Unofficial Interactive Sea Of Thieves Map";
-$keywords = "Sea of Thieves, Map, Interactive Map, Islands, Chickens, Snakes, Pigs, Outposts, Pirates";
-$version = "v1.12";
+$keywords = "Sea of Thieves, Map, Interactive Map, Islands, Chickens, Snakes, Pigs, Outposts, Pirates, Beacons, Thrones, Seaposts";
+$version = "v1.14";
 
-define('CACHE_BUSTER', '1545520036634');            // Automatically updates
+define('CACHE_BUSTER', '1546018678378');            // Automatically updates
 
 ?>
