60c351689d89095fc5261b6151325fd2317c9c38
Vince Suriani <chenzorama@hotmail.com>
2019-01-02 07:23:21 -0500

    Almost there.

--- www/includes/globals.php
+++ www/includes/globals.php
@@ -5,6 +5,6 @@ $title = "Unofficial Interactive Sea Of Thieves Map";
 $keywords = "Sea of Thieves, Map, Interactive Map, Islands, Chickens, Snakes, Pigs, Outposts, Pirates, Beacons, Thrones, Seaposts";
 $version = "v1.15";
 
-define('CACHE_BUSTER', '1546371874966');            // Automatically updates
+define('CACHE_BUSTER', '1546392541852');            // Automatically updates
 
 ?>
