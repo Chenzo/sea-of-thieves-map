008010676c440be62faf30d01b1dcf25ad5bec34
Vince Suriani <chenzorama@hotmail.com>
2019-02-07 15:41:01 -0500

    Added in text titles

--- www/includes/globals.php
+++ www/includes/globals.php
@@ -7,7 +7,7 @@ $default_language = "fr";
 $version = "v1.22";
 
 
-define('CACHE_BUSTER', '1549395678727');            // Automatically updates
+define('CACHE_BUSTER', '1549571852335');            // Automatically updates
 
 
 ?>
