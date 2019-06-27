2ba646998421bb08b34ffdffc24daeb813d6a1b6
Vince Suriani <chenzorama@hotmail.com>
2019-06-27 15:33:54 -0400

    more journal locations

--- src/js/modules/places_data.js
+++ src/js/modules/places_data.js
@@ -98,37 +98,114 @@ var places = [
         "loc": [-40.96286, 62.526276], 
         "title": "Briggsy Journal 1",
         "desc": "Inside the North West cave on Lone Cove. In on the right by some glowing mushrooms.",
-        "image" : "mercia_journal_5.jpg",
+        "image" : "briggsy_journal_01.jpg",
         talltale: 3
     },
     {
         "loc": [-95.986481, 15.967438], 
         "title": "Briggsy Journal 2",
         "desc": "in a broken barrel by a cooking fire on the North West shore of Mermaid's Hideaway",
-        "image" : "mercia_journal_5.jpg",
+        "image" : "briggsy_journal_02.jpg",
         talltale: 3
     },
     {
         "loc": [-137.257812, 44.59375], 
         "title": "Briggsy Journal 3",
         "desc": "in a hole in the bow of the wrecked ship on Old Salt's Atoll",
-        "image" : "mercia_journal_5.jpg",
+        "image" : "briggsy_journal_03.jpg",
         talltale: 3
     },
     {
         "loc": [-54.125, 105.515625], 
         "title": "Briggsy Journal 4",
         "desc": "in a bookshelf in the tavern on Dagger Tooth Outpost",
-        "image" : "mercia_journal_5.jpg",
+        "image" : "briggsy_journal_04.jpg",
         talltale: 3
     },
     {
         "loc": [-55.703125, 145.148438], 
         "title": "Briggsy Journal 5",
         "desc": "at the very top of the galleon on Galleons Grave in the cage with a skeleton",
-        "image" : "mercia_journal_5.jpg",
+        "image" : "briggsy_journal_05.jpg",
         talltale: 3
+    } , 
+    
+    
+    {
+        "loc": [-67.523437, 66.414063], 
+        "title": "Stars of a Thief journals – Sudds’ Notes 1",
+        "desc": "On the eastern most little island of Rum Runner Isle",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 4
+    },
+    {
+        "loc": [-117.914062, 52.132813], 
+        "title": "Stars of a Thief journals – Sudds’ Notes 2",
+        "desc": "On Plunder Valley right by the beacon",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 4
+    },
+    {
+        "loc": [-115.226562, 85.5], 
+        "title": "Stars of a Thief journals – Sudds’ Notes 3",
+        "desc": "North Eastern edge of the central island on Snake Island. Over a ledge.",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 4
+    },
+    {
+        "loc": [-142.4375, 127.9375], 
+        "title": "Stars of a Thief journals – Sudds’ Notes 4",
+        "desc": "At the tallest point on Devils Ridge. You'll need to climb up some rocks to get there.",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 4
+    },
+    {
+        "loc": [-67.453125, 15.375], 
+        "title": "Stars of a Thief journals – Sudds’ Notes 5",
+        "desc": "On a ridge overlooking the sea on Cresent Isle by some star gazing equipment",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 4
+    },
+
+
+
+    {
+        "loc": [-58.546875, 30.3046885], 
+        "title": "Wild Rose journals – Lovers’ Notes 1",
+        "desc": "Right by some barrels in the middle of Rapier Cay",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 5
+    },
+    {
+        "loc": [-70.140625, 50.640625], 
+        "title": "Wild Rose journals – Lovers’ Notes 2",
+        "desc": "in a barrel by the campfire on the beach of Cannon Cove",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 5
+    },
+    {
+        "loc": [-42.078125, 62.8125], 
+        "title": "Wild Rose journals – Lovers’ Notes 3",
+        "desc": "at the base of the giant boudler in the middle of Lone Cove",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 5
+    },
+    {
+        "loc": [-91.929687, 31.015625], 
+        "title": "Wild Rose journals – Lovers’ Notes 4",
+        "desc": "on top of a barrel on the south east side of Lagoon of Whispers",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 5
+    },
+    {
+        "loc": [-28.5, 19.765625], 
+        "title": "Wild Rose journals – Lovers’ Notes 4",
+        "desc": "by a couple barrels on one of the eastern islets at Sailor's Bounty",
+        "image" : "mercia_journal_5.jpg",
+        talltale: 5
     }  
+    
+    
     
     
 ];
